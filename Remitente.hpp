22df1df40bea42c4f26baa8047c7ba3a19c2001f
Darkas Voidtear <105468040+Darkas-Voidtear@users.noreply.github.com>
2023-04-30 13:49:45 -0500

    Update and rename Remitente.cpp to Remitente.hpp

---
+++ Remitente.hpp
@@ -0,0 +1,9 @@
+#include "Clientes.hpp"
+#include <iostream>
+
+template <typename T>
+class Remitente : public Clientes<T> {
+private:
+public:
+    Remitente(Ubicacion<T> ubicacion) : Clientes<T>(ubicacion) {}
+};
