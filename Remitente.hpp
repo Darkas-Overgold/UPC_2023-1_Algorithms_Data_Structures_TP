78caba519948107182c1a9a5385c69bf3bdce51c
Darkas Voidtear <105468040+Darkas-Voidtear@users.noreply.github.com>
2023-05-19 16:25:38 -0500

    Delete Remitente.hpp

--- Remitente.hpp
+++
@@ -1,9 +0,0 @@
-#include "Clientes.hpp"
-#include <iostream>
-
-template <typename T>
-class Remitente : public Clientes<T> {
-private:
-public:
-    Remitente(Ubicacion<T> ubicacion) : Clientes<T>(ubicacion) {}
-};
